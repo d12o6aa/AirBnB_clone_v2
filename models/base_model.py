b072fcc7114e7cc4c3f95956c0ce319e03d211ac
d12o6aa <doaakarem798@gmail.com>
2023-12-26 14:17:02 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -16,11 +16,9 @@ class BaseModel:
     created_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
     updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
 
-    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -58,7 +56,7 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-         res = {}
+        res = {}
         for key, value in self.__dict__.items():
             if key != '_sa_instance_state':
                 if isinstance(value, datetime):
